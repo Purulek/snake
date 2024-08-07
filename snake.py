6fc39c3c6ba4d8ac113cb2ec4d3d5217613df334
purulek <adam.purul@gmail.com>
2024-08-07 22:16:11 +0200

    sixth commit

--- snake.py
+++ snake.py
@@ -1,7 +1,9 @@
 import turtle
 import time
+import random
 t2 = turtle.Turtle()
 t1 = turtle.Turtle()
+
 
 
 class Board:
@@ -27,8 +29,11 @@ class Board:
 class Snake:
     def __init__(self) -> None:
         self.screen = turtle.Screen()
-        self.direction = ["sleep"] * 3
+        self.fruit =turtle.Turtle()
+        self.direction_of_t2 = ["sleep"] *5
+        self.direction_of_t1 =["forward"] 
         self.screen.tracer()
+        t1.shape("triangle")
 
     def __call__(self):
         t1.color("red")
@@ -37,7 +42,16 @@ class Snake:
         t2.home()
         
         t2.pendown()
-        
+    
+    def treat(self):
+        self.fruit.shape("circle")
+        self.fruit.color("white")
+        self.fruit.penup()
+        xcor = random.randrange(-200,200, 10)
+        ycor = random.randrange(-200,200, 10)
+        self.fruit.setpos(xcor,ycor)
+        self.fruit.pendown()
+        self.fruit.color("yellow")
        
         
         
@@ -45,52 +59,83 @@ class Snake:
     def move(self):
         t1.speed(1)
         t1.forward(10)
-        print("t1 forward")
-        #print(t1.ycor())
-        #print(t1.xcor())
-        self.direction.append("forward") 
+        #print("t1 forward")
+        self.direction_of_t2.append("forward") 
         if t1.ycor() >= 200 or t1.ycor() <= -200 or t1.xcor() >= 200 or t1.xcor() <= -200:
             print("You lose")
-            for word in self.direction:
-                print (word)
             turtle.bye()
+        elif abs(t1.ycor()) >= abs(self.fruit.ycor())-1 and abs(t1.ycor()) <= abs(self.fruit.ycor())+ 1 and abs(t1.xcor()) >= abs(self.fruit.xcor()) -1 and abs(t1.xcor()) <= abs(self.fruit.xcor()) + 1 :
+            self.direction_of_t2.append("sleep")
+            self.direction_of_t2.append("sleep")
+            self.fruit.color("white")
             
         
         
 
     def go_left(self, dummy=None):
         t1.left(90)
-        print("t1 - left")
-        self.direction.append("left") 
+        #print("t1 - left")
+        self.direction_of_t2.append("left") 
 
 
     def go_right(self, dummy=None):
         t1.right(90)
-        print("t1 right")
-        self.direction.append("right") 
+        #print("t1 right")
+        self.direction_of_t2.append("right") 
 
+
+    def left(self):
+        del self.direction_of_t1[-1]
+        
+        self.direction_of_t1.append("left")
+        
+    
+    def right(self):
+        del self.direction_of_t1[-1]
+        
+        self.direction_of_t1.append("right")
+    
+    def forward(self):
+        self.direction_of_t1.append("forward")
+        
 
     def turnig (self):
         i = 0
          
-        while i < 500:
-              for action in self.direction :
-                print (action)
-                if action =="left" :
+        while i < 1:
+              
+              for action_of_t1, action_of_t2 in zip(self.direction_of_t1 ,self.direction_of_t2) :
+                #print ("t1",action_of_t1)
+                #print ("t2",action_of_t2)
+
+                if len(self.direction_of_t1) % 40 == 0:
+                    self.treat()
+                if action_of_t2 =="left" :
                     t2.left(90)
-                elif action == "right":
+                elif action_of_t2 == "right":
                     t2.right(90)
-                elif action == "forward":
+                elif action_of_t2 == "forward":
                     t2.forward(10)
-                elif action == "sleep":
+                elif action_of_t2 == "sleep":
                     pass
                 self.screen.listen()
-                self.screen.ontimer(self.move(),250)
-                self.screen.onkey(self.go_left, "Left")
-                self.screen.onkey(self.go_right, "Right")
-                self.screen.onkey(turtle.bye, "Up")
+                self.screen.onkey(self.left, "Left")
+                self.screen.onkey(self.right, "Right")
+                
+                self.screen.ontimer(self.forward(),250)
+
+                if action_of_t1 == "forward":
+                    self.screen.ontimer(self.move(),250)
+                elif action_of_t1 == "left":
+                    self.go_left()
+                elif action_of_t1 == "right":
+                    self.go_right()
                 
                 i +=1 
+              
+                
+
+                
 
 
 
